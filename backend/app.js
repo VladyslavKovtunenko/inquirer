d1a27b8f364aab41357b42f7f53ce90b91d63850
vadimkostenko <er.coooool@gmail.com>
2016-05-12 20:35:43 +0300

    Done add question by (POST).

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
 var http = require('http');
 var path = require('path');
 var config = require('lib/config');
@@ -11,6 +12,7 @@ var app = express();
 app.set('views', path.join(__dirname, '../frontend/view'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '../frontend/static')));
+app.use(bodyParser.json());
 
 http.createServer(app).listen(config.get('server_port'), function () {
     log.info('Express start on port ' + config.get('server_port'));
@@ -21,38 +23,30 @@ app.use(function (req, res) {
     switch (req.url) {
         case '/':
             /* (GET) get view */
-            // res.render('index');
             res.sendFile('/home/someone/Projects/js/inquirer/frontend/view/index.html');
             break;
         case '/api/questions':
             /* (GET) get questions list */
-            dao.get().then(function (arr) {
-                if (arr.length > 1) {
+            if (req.method == 'GET') {
+                dao.get().then(function (arr) {
                     var validObj = generateValidObj(arr);
-                    res.send(validObj);
-                } else {
-                    res.send(arr);
-                }
-            });
+                    res.writeHead(200, {
+                        'Content-Type': 'application/json'
+                    });
+                    res.write(validObj);
+                    res.end();
+                });
+            } else {
+                res.send(400, 'Bad request!');
+            }
             break;
         case '/api/question':
             /* (POST) add new question */
-            dao.send(JSON.stringify({
-                fields: [
-                    {
-                        type: 'short_text',
-                        question: 'How are you?'
-                    },
-                    {
-                        type: 'rating',
-                        question: 'Rating?',
-                        range: {
-                            start: '1',
-                            end: '10'
-                        }
-                    }
-                ]
-            }));
+            if (req.method == 'POST') {
+                dao.send(JSON.stringify(req.body));
+            } else {
+                res.send(400, 'Bad request!');
+            }
             res.end();
             break;
         case '/api/question/id':
@@ -71,12 +65,17 @@ function generateValidObj(arr) {
         fields: []
     };
 
-    arr.forEach(function (item) {
-        var tempArr = JSON.parse(item).fields;
-        tempArr.forEach(function (item) {
-            obj.fields.push(item);
+    if (arr.length > 1) {
+        arr.forEach(function (item) {
+            var tempArr = JSON.parse(item).fields;
+            tempArr.forEach(function (item) {
+                obj.fields.push(item);
+            });
         });
-    });
+    } else {
+        var tempArr = JSON.parse(arr[0]);
+        obj.fields.push(tempArr);
+    }
 
     return JSON.stringify(obj);
 }
--- frontend/static/js/init.js
+++ frontend/static/js/init.js
@@ -1,10 +1,52 @@
 ;(function ($) {
-    $.ajax({
-        url: 'http://localhost:3000/api/questions',
-        type: 'GET',
-        dataType: 'json',
-        success: function (data) {
-            $().inquirer('#add', JSON.stringify(data));
-        }
-    });
+    getQuestionList();
+    // addQuestion();
+
+    function getQuestionList() {
+        $.ajax({
+            url: 'http://localhost:3000/api/questions',
+            type: 'GET',
+            contentType: 'application/json',
+            dataType: 'json',
+            success: function (data) {
+                $().inquirer('#add', JSON.stringify(data));
+            }
+        });
+    }
+
+    function addQuestion() {
+        var questionData = {
+            type: 'multiple_choice',
+            question: 'Who are you?',
+            choices: [
+                {
+                    label: 'iron-man'
+                },
+                {
+                    label: 'batman'
+                },
+                {
+                    label: 'cat'
+                },
+                {
+                    label: 'human'
+                },
+                {
+                    label: 'lol'
+                },
+                {
+                    label: 'John'
+                }
+            ]
+        };
+        
+        $.ajax({
+            url: 'http://localhost:3000/api/question',
+            type: 'POST',
+            contentType: 'application/json',
+            dataType: 'json',
+            data: JSON.stringify(questionData)
+        });
+    }
+
 })(jQuery);
